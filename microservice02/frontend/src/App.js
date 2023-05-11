298ce11ef922fb55b6f393a2ff79c259ff0a68e2
Master10000000 <el17878@mail.ntua.gr>
2023-05-11 20:08:33 +0300

    Logout Page

--- microservice02/frontend/src/App.js
+++ microservice02/frontend/src/App.js
@@ -4,6 +4,7 @@ import About from "./Components/Pages/AboutPage/AboutPage";
 import Toolbar from "./Components/Navigation/Toolbar/Toolbar";
 import Footer from "./Components/Navigation/Footer/Footer";
 import Login from "./Containers/LoginPage/LoginPage";
+import Logout from "./Containers/LogoutPage";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <Toolbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />        
+        <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
+        <Route path="logout" element={<Logout />} />
       </Routes>
       <Footer />
     </div>
