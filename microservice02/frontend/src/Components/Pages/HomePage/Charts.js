018571ea48306dd17d3bfc940a5374fa591b305a
Master10000000 <el17878@mail.ntua.gr>
2023-05-13 13:01:08 +0300

    Dependency wheel Diagram

--- microservice02/frontend/src/Components/Pages/HomePage/Charts.js
+++ microservice02/frontend/src/Components/Pages/HomePage/Charts.js
@@ -3,10 +3,15 @@ import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import classes from "./HomePage";
 import Button from "@mui/material/Button";
+import HC_sankey from "highcharts/modules/sankey";
+import HC_depwheel from "highcharts/modules/dependency-wheel";
+
+HC_sankey(Highcharts);
+HC_depwheel(Highcharts);
 
 const Charts = () => {
   const [chartIndex, setChartIndex] = useState(0);
-  const chartOptions1 = {
+  const PieDiagram = {
     chart: {
       plotBackgroundColor: null,
       plotBorderWidth: null,
@@ -82,8 +87,88 @@ const Charts = () => {
       },
     ],
   };
+  const DependencyWheel = {
+    title: {
+      text: "Highcharts Dependency Wheel",
+    },
 
-  const chartOptions2 = {
+    accessibility: {
+      point: {
+        valueDescriptionFormat:
+          "{index}. From {point.from} to {point.to}: {point.weight}.",
+      },
+    },
+
+    series: [
+      {
+        keys: ["from", "to", "weight"],
+        data: [
+          ["Brazil", "Portugal", 5],
+          ["Brazil", "France", 1],
+          ["Brazil", "Spain", 1],
+          ["Brazil", "England", 1],
+          ["Canada", "Portugal", 1],
+          ["Canada", "France", 5],
+          ["Canada", "England", 1],
+          ["Mexico", "Portugal", 1],
+          ["Mexico", "France", 1],
+          ["Mexico", "Spain", 5],
+          ["Mexico", "England", 1],
+          ["USA", "Portugal", 1],
+          ["USA", "France", 1],
+          ["USA", "Spain", 1],
+          ["USA", "England", 5],
+          ["Portugal", "Angola", 2],
+          ["Portugal", "Senegal", 1],
+          ["Portugal", "Morocco", 1],
+          ["Portugal", "South Africa", 3],
+          ["France", "Angola", 1],
+          ["France", "Senegal", 3],
+          ["France", "Mali", 3],
+          ["France", "Morocco", 3],
+          ["France", "South Africa", 1],
+          ["Spain", "Senegal", 1],
+          ["Spain", "Morocco", 3],
+          ["Spain", "South Africa", 1],
+          ["England", "Angola", 1],
+          ["England", "Senegal", 1],
+          ["England", "Morocco", 2],
+          ["England", "South Africa", 7],
+          ["South Africa", "China", 5],
+          ["South Africa", "India", 1],
+          ["South Africa", "Japan", 3],
+          ["Angola", "China", 5],
+          ["Angola", "India", 1],
+          ["Angola", "Japan", 3],
+          ["Senegal", "China", 5],
+          ["Senegal", "India", 1],
+          ["Senegal", "Japan", 3],
+          ["Mali", "China", 5],
+          ["Mali", "India", 1],
+          ["Mali", "Japan", 3],
+          ["Morocco", "China", 5],
+          ["Morocco", "India", 1],
+          ["Morocco", "Japan", 3],
+          ["Japan", "Brazil", 1],
+        ],
+        type: "dependencywheel",
+        name: "Dependency wheel series",
+        dataLabels: {
+          color: "#333",
+          style: {
+            textOutline: "none",
+          },
+          textPath: {
+            enabled: true,
+          },
+          distance: 10,
+        },
+        size: "95%",
+      },
+    ],
+  };
+
+  const LineDiagram = {
     chart: {
       type: "line",
       title: {
@@ -169,27 +254,10 @@ const Charts = () => {
           ],
         },
       ],
-
-      responsive: {
-        rules: [
-          {
-            condition: {
-              maxWidth: 500,
-            },
-            chartOptions: {
-              legend: {
-                layout: "horizontal",
-                align: "center",
-                verticalAlign: "bottom",
-              },
-            },
-          },
-        ],
-      },
     },
   };
 
-  const chartOptions3 = {
+  const ColumnDiagram = {
     chart: {
       type: "column",
     },
@@ -269,7 +337,7 @@ const Charts = () => {
     ],
   };
 
-  const chartsList = [chartOptions1, chartOptions2, chartOptions3];
+  const chartsList = [PieDiagram, LineDiagram, ColumnDiagram, DependencyWheel];
 
   const handleChartChange = (index) => {
     setChartIndex(index);
@@ -297,6 +365,13 @@ const Charts = () => {
         onClick={() => handleChartChange(2)}
       >
         Column
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => handleChartChange(3)}
+      >
+        Dependency Wheel
       </Button>
       <p className={classes.mainbody}>
         <p>
