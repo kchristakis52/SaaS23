fc44f1e95cf91fe90ba6803e6521f28447a30599
ioalexandros <ioalexandros@hotmail.com>
2023-05-16 12:49:17 +0300

    collect & mv info from footer

--- microservice02/frontend/src/Components/Pages/AboutPage/AboutPage.js
+++ microservice02/frontend/src/Components/Pages/AboutPage/AboutPage.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import classes from './AboutPage.module.css';
+import React from "react";
+import classes from "./AboutPage.module.css";
 
 const About = () => (
   <div className={classes.mainbody}>
-    <h2>
-      About us:
-      </h2>
+    <h2>About us:</h2>
     <p>
-    This website was created for the semester project of the course: <br />
+      This website was created for the semester project of the course: <br />
       "Software Technology Services" of NTUA ECE School.
       <br /> Course is currently in the 8th semester.
       <br /> <br />
-      Our team consists of: <br />  Panagiotis Hatzigiannakis, 
-      Alexandros Ioannidis , Konstantinos Christakis , Konstantinos Kefalas . 
-      <br /> The github repository for this whole project is in the navigation bar of
-      the page.
+      Our team consists of: <br /> Panagiotis Hatzigiannakis, Alexandros
+      Ioannidis , Konstantinos Christakis , Konstantinos Kefalas .
+      <br /> The github
+      <a href="https://github.com/ntua/SaaS23-20">repository</a>.
     </p>
   </div>
 );
