afb05f96cac9ce6612d1aa760805c27cd063f0e8
Master10000000 <el17878@mail.ntua.gr>
2023-05-25 21:46:22 +0300

    Create two extra diagrams

--- microservice02/frontend/src/Components/Pages/CreateChartPage/CreateChartPage.js
+++ microservice02/frontend/src/Components/Pages/CreateChartPage/CreateChartPage.js
@@ -4,33 +4,40 @@ import LineDiagram from "../../../Assets/images/line_diagram.png";
 import PieDiagram from "../../../Assets/images/pie-diagram.png";
 import ColumnDiagram from "../../../Assets/images/column-diagram.png";
 import DependencyWheelDiagram from "../../../Assets/images/dependency-wheel-diagram.png";
+import LineWithAnnotationsDiagram from "../../../Assets/images/line-with-annotations-diagram.png";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 const CreateChart = () => {
+  const containerStyles = {
+    width: "500px",
+    height: "300px",
+    margin: "auto",
+    border: "1px solid #ccc",
+  };
   return (
     <div className={classes.mainbody}>
       <h2>Let's create your own chart !</h2>
-      <Carousel
-        className="carousel-container"
-        showArrows={true}
-        autoPlay={true}
-        infiniteLoop={true}
-      >
-        <div className="carousel-item">
-          <img src={PieDiagram}></img>
-        </div>
-        <div className="carousel-item">
-          <img src={LineDiagram}></img>
-        </div>
-        <div className="carousel-item">
-          <img src={ColumnDiagram}></img>
-        </div>
-        <div className="carousel-item">
-          <img src={DependencyWheelDiagram}></img>
-        </div>
-      </Carousel>
-      <p>Download chart description template for (selected type)</p>
+      <div style={containerStyles}>
+        <Carousel showArrows={true} autoPlay={true} infiniteLoop={true}>
+          <div>
+            <img src={PieDiagram}></img>
+          </div>
+          <div>
+            <img src={LineDiagram}></img>
+          </div>
+          <div>
+            <img src={ColumnDiagram}></img>
+          </div>
+          <div>
+            <img src={DependencyWheelDiagram}></img>
+          </div>
+          <div>
+            <img src={LineWithAnnotationsDiagram}></img>
+          </div>
+        </Carousel>
+        <p>Download chart description template for (selected type)</p>
+      </div>
     </div>
   );
 };
