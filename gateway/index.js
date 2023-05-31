f76831c7016efd9fd35de8ffeb76b443edcd03f2
Konstantinos Christakis <el19646@mail.ntua.gr>
2023-05-31 15:30:01 +0300

    Delete comments

--- gateway/index.js
+++ gateway/index.js
@@ -22,19 +22,6 @@ app.post('/parse-csv', upload.single('csv'), (req, res) => {
     let chartname = req.file.originalname;
     chartname = chartname.slice(0, chartname.length - 4);
     //console.log(chartname)
-    // const results = [];
-
-    // // Read the CSV file using the csv-parser library
-    // fs.createReadStream(req.file.path)
-    //   .pipe(csv())
-    //   .on('data', (data) => {
-    //     // Process each row of the CSV
-    //     results.push(data);
-    //   })
-    //   .on('end', () => {
-    //     // Send the parsed CSV data as the API response
-    //     res.status(200).json({ [chartname]: results });
-    //   });
     console.log(req.file.buffer.toString('utf-8'));
     res.status(200).json({ [chartname]: req.file.buffer.toString('utf-8') })
   } catch (err) {
